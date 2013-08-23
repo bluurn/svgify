0b46714bcd0c93ca96c3d50c1564c939c5aeaaf1
Vladimir Suvorov <v.suvorov@itech-group.ru>
2013-08-23 16:48:45 +0400

    svg support checking added

--- js/jquery.svgify.js
+++ js/jquery.svgify.js
@@ -1,41 +1,43 @@
 (function ($) {
 	$.fn.svgify = function () {
+		var isSVGSupported = document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1");
+		if (isSVGSupported) {
+			$(this).each(function () {
 
-		$(this).each(function () {
+				var elt = $(this);
 
-			var elt = $(this);
+				if (!elt.has('data-svgsrc')) {
+					return;
+				}
+				var 
+					svgsrc = elt.data('svgsrc'),
+					w = elt.data('w'),
+					h = elt.data('h');
 
-			if (!elt.has('data-svgsrc')) {
-				return;
-			}
-			var 
-				svgsrc = elt.data('svgsrc'),
-				w = elt.data('w'),
-				h = elt.data('h');
+				if (elt.prop('tagName') === "IMG") {
+					elt
+					   .attr('src', svgsrc)
+					   .css({
+						width: w,
+						height: h
+					   })
+					   .removeAttr('data-svgsrc');
 
-			if (elt.prop('tagName') === "IMG") {
-				elt
-				   .attr('src', svgsrc)
-				   .css({
-					width: w,
-					height: h
-				   })
-				   .removeAttr('data-svgsrc');
+				}
 
-			}
-
-			if(elt.prop('tagName') !== "IMG" && !elt.children().length) {
-				var w = elt.width(),
-				    h = elt.height()
-				elt
-				   .css({background: 'none'})
-				   .html('<img src="' + svgsrc + '" />')
-				   .removeAttr('data-svgsrc');
-				elt
-				   .find('img')
-				   .css({ width: w, height: h });
-			}
-		});
+				if(elt.prop('tagName') !== "IMG" && !elt.children().length) {
+					var w = elt.width(),
+					    h = elt.height()
+					elt
+					   .css({background: 'none'})
+					   .html('<img src="' + svgsrc + '" />')
+					   .removeAttr('data-svgsrc');
+					elt
+					   .find('img')
+					   .css({ width: w, height: h });
+				}
+			});
+		}
 
 		return this;
 	};
